5d1360446137c10693640e445e92d9c50da697a3
Rizka Aulia <fauziahrizka06@gmail.com>
2025-10-21 20:08:13 +0700

    Modul 2

--- app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -1,20 +1,48 @@
 package lat.pam.droidcafe
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class OrderActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var addressEditText: EditText
+    private lateinit var phoneEditText: EditText
+    private lateinit var cityEditText: EditText
+    private lateinit var noteEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_order)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Hubungkan dengan komponen di layout (sesuai ID di XML)
+        nameEditText = findViewById(R.id.name_text)
+        addressEditText = findViewById(R.id.address_text)
+        phoneEditText = findViewById(R.id.phone_text)
+        cityEditText = findViewById(R.id.editText_city)
+        noteEditText = findViewById(R.id.editText_note)
+    }
+
+    // Ketika radio button diklik
+    fun onRadioButtonClicked(view: View) {
+        val checked = (view as RadioButton).isChecked
+        when (view.id) {
+            R.id.sameday -> if (checked)
+                displayToast(getString(R.string.same_day_messenger_service))
+            R.id.nextday -> if (checked)
+                displayToast(getString(R.string.next_day_ground_delivery))
+            R.id.pickup -> if (checked)
+                displayToast(getString(R.string.pick_up))
         }
+    }
+
+
+
+    private fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 }
